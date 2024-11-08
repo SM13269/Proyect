ac0cfad666a5844591a3f41f2b9f2780a248ccb1
AnthoP4023 <anthonyp200330@gmail.com>
2024-11-08 18:53:02 -0500

    first commit

--- src/db.js
+++ src/db.js
@@ -1,12 +1,9 @@
 import { createPool } from "mysql2/promise";
-import {BD_HOST, DB_USER, DB_PASSWORD, BD_DATABASE, DB_PORT, CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET} from './config.js'
+import {BD_HOST, DB_USER, DB_PASSWORD, BD_DATABASE, DB_PORT} from './config.js'
 export const conmysql=createPool({
     host: BD_HOST,
     database: BD_DATABASE,
     user: DB_USER,
     password:DB_PASSWORD,
     port:DB_PORT,
-    cloudinary_cloud_name:CLOUDINARY_CLOUD_NAME,
-    cloudinary_cloud_name_apy_key:CLOUDINARY_API_KEY,
-    cloudinary_api_secret:CLOUDINARY_API_SECRET
 })
